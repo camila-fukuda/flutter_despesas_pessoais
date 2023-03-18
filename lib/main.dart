0a8f7fa050b3e0bc1032d8ac6e4bfc98e60e9004
Camila Fukuda <camilaffukuda@gmail.com>
2023-03-18 14:32:02 -0300

    Adicionados os componentes para o gráfico de semana e para a lista de transações.

--- lib/main.dart
+++ lib/main.dart
@@ -22,8 +22,22 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Despesas Pessoais'),
       ),
-      body: const Center(
-        child: Text('Versão inicial.'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: const <Widget>[
+          SizedBox(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              elevation: 5,
+              child: Text('Gráfico dias da semana.'),
+            ),
+          ),
+          Card(
+            child: Text('Lista de transações.'),
+          )
+        ],
       ),
     );
   }
