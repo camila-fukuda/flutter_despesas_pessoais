e7b9eb89327bcd91c5f3f13f97df1ffb4d9a4599
Camila Fukuda <camilaffukuda@gmail.com>
2023-03-19 16:08:12 -0300

    Usando o listView.

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -9,75 +9,80 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        children: transactions
-            .map((tr) => Card(
-                  child: Row(
-                    children: [
-                      Flexible(
-                        flex: 1,
-                        child: Container(
+    return SizedBox(
+      height: 500,
+      child: ListView.builder(
+        itemCount: transactions.length,
+        itemBuilder: (ctx, index) {
+          final tr = transactions[index];
+          return Card(
+            child: Row(
+              children: [
+                Flexible(
+                  flex: 1,
+                  child: Container(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 15, horizontal: 5),
+                    decoration: BoxDecoration(
+                      color: Colors.purple,
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 10,
+                      vertical: 5,
+                    ),
+                    child: Center(
+                      child: Text(
+                        'R\$ ${tr.value.toStringAsFixed(2)}',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Flexible(
+                  flex: 2,
+                  child: Container(
+                    margin: const EdgeInsets.fromLTRB(5, 10, 10, 10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
                           padding: const EdgeInsets.symmetric(
-                              vertical: 15, horizontal: 5),
-                          decoration: BoxDecoration(
-                            color: Colors.purple,
-                            borderRadius: BorderRadius.circular(25),
+                            vertical: 3,
                           ),
-                          margin: const EdgeInsets.symmetric(
-                            horizontal: 10,
-                            vertical: 5,
-                          ),
-                          child: Center(
-                            child: Text(
-                              'R\$ ${tr.value.toStringAsFixed(2)}',
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                                color: Colors.white,
-                              ),
+                          child: Text(
+                            tr.title,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
                         ),
-                      ),
-                      Flexible(
-                        flex: 2,
-                        child: Container(
-                          margin: const EdgeInsets.fromLTRB(5, 10, 10, 10),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.symmetric(
-                                  vertical: 3,
-                                ),
-                                child: Text(
-                                  tr.title,
-                                  style: const TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.symmetric(
-                                  vertical: 3,
-                                ),
-                                child: Text(
-                                  DateFormat('d MMM y').format(tr.date),
-                                  style: const TextStyle(
-                                      fontSize: 14,
-                                      color:
-                                          Color.fromARGB(255, 121, 121, 121)),
-                                ),
-                              ),
-                            ],
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 3,
+                          ),
+                          child: Text(
+                            DateFormat('d MMM y').format(tr.date),
+                            style: const TextStyle(
+                                fontSize: 14,
+                                color: Color.fromARGB(255, 121, 121, 121)),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ))
-            .toList());
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
