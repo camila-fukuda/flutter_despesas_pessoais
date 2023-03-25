00a95f34f5bda85f2c288e4c21203ce9f552c59f
Camila Fukuda <camilaffukuda@gmail.com>
2023-03-25 13:02:39 -0300

    Criando date picker.

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class TransactionForm extends StatefulWidget {
   final void Function(String, double) onSubmit;
@@ -10,13 +11,13 @@ class TransactionForm extends StatefulWidget {
 }
 
 class _TransactionFormState extends State<TransactionForm> {
-  final titleController = TextEditingController();
-
-  final valueController = TextEditingController();
+  final _titleController = TextEditingController();
+  final _valueController = TextEditingController();
+  DateTime? _selectedDate;
 
   _submitForm() {
-    final title = titleController.text;
-    final value = double.tryParse(valueController.text.replaceAll(",", "."));
+    final title = _titleController.text;
+    final value = double.tryParse(_valueController.text.replaceAll(",", "."));
 
     if (title.isEmpty || value == null) {
       print('empty');
@@ -24,6 +25,23 @@ class _TransactionFormState extends State<TransactionForm> {
     }
 
     widget.onSubmit(title, value);
+  }
+
+  _showDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2019),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
   }
 
   @override
@@ -35,7 +53,7 @@ class _TransactionFormState extends State<TransactionForm> {
         child: Column(
           children: [
             TextField(
-              controller: titleController,
+              controller: _titleController,
               onSubmitted: (_) => _submitForm(),
               decoration: const InputDecoration(
                 labelText: 'Título',
@@ -44,21 +62,52 @@ class _TransactionFormState extends State<TransactionForm> {
             TextField(
               keyboardType:
                   const TextInputType.numberWithOptions(decimal: true),
-              controller: valueController,
+              controller: _valueController,
               onSubmitted: (_) => _submitForm(),
               decoration: const InputDecoration(
                 labelText: 'Valor',
               ),
             ),
             Padding(
-              padding: const EdgeInsets.fromLTRB(0, 15, 0, 5),
+              padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 0),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
+                  Expanded(
+                    child: Text(
+                      _selectedDate == null
+                          ? 'Nenhuma data selecionada!'
+                          : 'Data Selecionada: ${DateFormat('dd/MM/y').format(_selectedDate!)}',
+                    ),
+                  ),
                   TextButton(
-                      onPressed: _submitForm, child: const Text('Adicionar')),
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                            const Color.fromARGB(255, 214, 214, 214)),
+                        foregroundColor: MaterialStateProperty.all(
+                            const Color.fromARGB(255, 63, 63, 63))),
+                    onPressed: _showDatePicker,
+                    child: const Text(
+                      'Selecionar uma data.',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
                 ],
               ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton(
+                  onPressed: _submitForm,
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                        Theme.of(context).colorScheme.primary),
+                  ),
+                  child: const Text('Adicionar Transação'),
+                ),
+              ],
             ),
           ],
         ),
@@ -66,3 +115,84 @@ class _TransactionFormState extends State<TransactionForm> {
     );
   }
 }
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:intl/intl.dart';
+
+// void main() {
+//   runApp(MyApp()); 
+// }
+
+// class MyApp extends StatelessWidget{
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         title: "Test App",
+//         home: HomePage(),
+//     );
+//   }
+// }
+
+// class HomePage extends StatefulWidget{
+//   @override
+//   State<StatefulWidget> createState() {
+//     return _HomePage();
+//   }
+// }
+
+// class _HomePage extends State<HomePage>{
+//   TextEditingController dateinput = TextEditingController(); 
+//   //text editing controller for text field
+  
+//   @override
+//   void initState() {
+//     dateinput.text = ""; //set the initial value of text field
+//     super.initState();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//             title:Text("DatePicker on TextField"), 
+//             backgroundColor: Colors.redAccent, //background color of app bar
+//         ),
+//         body:Container(
+//           padding: EdgeInsets.all(15),
+//           height:150,
+//           child:Center( 
+//              child:TextField(
+//                 controller: dateinput, //editing controller of this TextField
+//                 decoration: InputDecoration( 
+//                    icon: Icon(Icons.calendar_today), //icon of text field
+//                    labelText: "Enter Date" //label text of field
+//                 ),
+//                 readOnly: true,  //set it true, so that user will not able to edit text
+//                 onTap: () async {
+//                   DateTime pickedDate = await showDatePicker(
+//                       context: context, initialDate: DateTime.now(),
+//                       firstDate: DateTime(2000), //DateTime.now() - not to allow to choose before today.
+//                       lastDate: DateTime(2101)
+//                   );
+                  
+//                   if(pickedDate != null ){
+//                       print(pickedDate);  //pickedDate output format => 2021-03-10 00:00:00.000
+//                       String formattedDate = DateFormat('yyyy-MM-dd').format(pickedDate); 
+//                       print(formattedDate); //formatted date output using intl package =>  2021-03-16
+//                         //you can implement different kind of Date Format here according to your requirement
+
+//                       setState(() {
+//                          dateinput.text = formattedDate; //set output date to TextField value. 
+//                       });
+//                   }else{
+//                       print("Date is not selected");
+//                   }
+//                 },
+//              )
+//           )
+//         )
+//     );
+//   }
+// }
